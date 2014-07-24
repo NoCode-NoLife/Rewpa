94e85ff29baa84ce82f2eb78197e46e52fd7b3ce
exec <exec@mabinoger.de>
2014-07-24 20:24:47 +0200

    Changed license header

--- wRAPE/Program.cs
+++ wRAPE/Program.cs
@@ -1,6 +1,4 @@
-﻿// Copyright (C) 2014 Aura Project
-// 
-// Written by exec (exec@mabinoger.de)
+﻿// wRAPE - World data converter
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
